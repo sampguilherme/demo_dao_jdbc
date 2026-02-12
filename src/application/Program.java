c5d0b7f9f552127c714c153915ba1f5d48e7f3b4
Guilherme Sampaio <guilherme.sampaio324@hotmail.com>
2026-02-11 21:34:36 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,10 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Updated completed!");
+		
+		System.out.println("\n=== TEST 6: seller delete ===");
+		sellerDao.deleteById(1);
+		System.out.println("Delete completed!");
 	}
 
 }
